eb1466c56567fddc078a6c741b21046ec94e44cb
hoanglonglch <hoanglonglch@gmail.com>
2019-07-17 22:50:04 +0700

    Apply change in search box

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, {Component} from 'react';
 import CardList from "./CardList";
 import {robots} from "./robotsConstant";
 import SearchBox from "./SearchBox";
 
-const App = (props)=>{
-    return(
-        <div className="tc">
-            <h1>Robos Friends</h1>
-            <SearchBox/>
-            <CardList robots={robots}/>
-        </div>
-    );
+const state = {
+
 };
+
+class App extends Component{
+
+    constructor() {
+        super();
+        this.state = {
+            robots: robots,
+            searchFiled: ''
+        }
+    }
+
+    onSearchChange = (event) =>{
+        this.setState({searchField: event.target.value});
+    }
+
+    render(){
+        // bug
+        const filteredRobots =  this.state.robots.filter(robot =>{
+            console.log(robot.name.toLowerCase().includes(this.state.searchFiled.toLowerCase()), robot.name);
+            return robot.name.toLowerCase().includes(this.state.searchFiled.toLowerCase())
+        });
+
+        console.log('filteredRobots', filteredRobots);
+        return(
+            <div className="tc">
+                <h1>Robos Friends</h1>
+                <SearchBox
+                    searchField={this.state.searchFiled}
+                    searchChange={this.onSearchChange}/>
+                <CardList robots={robots}/>
+            </div>
+        );
+    }
+}
 
 export default App;
 
