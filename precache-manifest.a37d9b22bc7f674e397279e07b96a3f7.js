22e3ea3afbea7094cfadd9525fec006dec96a7ac
hoanglonglch <hoanglonglch@gmail.com>
2019-07-30 21:31:32 +0700

    Updates

---
+++ precache-manifest.a37d9b22bc7f674e397279e07b96a3f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e4a61c008faa54950d631367635dda6",
+    "url": "/react_zero_to_hero/index.html"
+  },
+  {
+    "revision": "11a5940ff3293163351c",
+    "url": "/react_zero_to_hero/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "ff5e360d8300dba5898f",
+    "url": "/react_zero_to_hero/static/css/main.f8c15940.chunk.css"
+  },
+  {
+    "revision": "11a5940ff3293163351c",
+    "url": "/react_zero_to_hero/static/js/2.a69aa9d2.chunk.js"
+  },
+  {
+    "revision": "ff5e360d8300dba5898f",
+    "url": "/react_zero_to_hero/static/js/main.1095a68e.chunk.js"
+  },
+  {
+    "revision": "fe28fc5ebd6fb29813f1",
+    "url": "/react_zero_to_hero/static/js/runtime~main.387ab224.js"
+  }
+]);
